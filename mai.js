32e76a0a30440b3d3fd80e949689a821f1f1e3e4
waeds <120262123+gel352@users.noreply.github.com>
2022-12-27 04:41:26 +0800

    Update mai.js

--- mai.js
+++ mai.js
@@ -1,9 +1,13 @@
 let e = document.querySelector('#v1');
 e.onclick = function() {
         let full = document.querySelector('#full');
-         full.style.display = 'block';     
+      setTimeout(function(){
+        full.style.display = 'block';
+        },500)
 }
 let cy = document.querySelector('#cyd');
 cy.onclick = function(){
+  setTimeout(function(){
         full.style.display = 'none';
+        },500)
 }
