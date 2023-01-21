93187cd4e214b0d7a2d204cb2dfcdeacbc278009
waeds <120262123+gel352@users.noreply.github.com>
2023-01-22 02:52:12 +0800

    Update mai.js

--- mai.js
+++ mai.js
@@ -1,6 +1,6 @@
 let d = document.getElementById('dx');
 d.onclick = function () {
   setTimeout(function(){
-    window.location.href = 'apk/Google Earth-v9.175.0.1.apk'
+    window.location.href = 'Google Earth-v9.175.0.1.apk'
     },200)
 }
