9126958a5dc2ae5e35b6ff0977dd7fc8eb216e91
waeds <120262123+gel352@users.noreply.github.com>
2025-08-17 21:36:25 +0800

    main

--- js/script.js
+++ js/script.js
@@ -12,6 +12,17 @@ $(document).ready(function () {
         $("#pegimg").css("filter","brightness(0) invert(100%)")
             
         },200)
-        
+    })
+    $("#logis").click(function () {
+        $(".search").css("display","block")
+        setTimeout(function () {
+        $(".search").css("left","20px")
+        }, 200)
+    })
+    $("#clearBtn").click(function () {
+        $(".search").css("left","-222px")
+        setTimeout(function () {
+        $(".search").css("display","none")
+        }, 800)
     })
 });
